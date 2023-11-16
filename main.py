d5e2e4f975685a043d9320d12bfd1e1456220cef
hector-teke <hectorguzman422@uma.es>
2023-11-16 18:54:54 +0100

    Definition of class Individual and redefinition of create_first method (it now generates directly real values)

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import random
 
 # rand/1/bin        CR = 0.3; F = 0.5
 
-def create_first(length=50, population=50):  # Returns the first population of solutions
+class Individual:
+    def __init__(self):
+        self.values = []
+        self.quality = None
+        self.mutationCons = None
+        self.crossoverProb = None
+
+    def __str__(self):
+        return f"values={self.values}, quality={self.quality}, mutationCons={self.mutationCons}, crossoverProb={self.crossoverProb})"
+
+def create_first(bound=5.12, population=20, dimension=10):  # Returns the first population of solutions
     solutions = []
 
     for i in range(population):
-        s = ""
-        for j in range(length):
-            s += str(random.randint(0, 1))
+        ind = Individual()
+        for j in range(dimension):
+            ind.values.append(random.uniform(- bound, bound))
 
-        solutions.append(s)
+        solutions.append(ind)
+        print(solutions[i])
 
     return solutions
 
@@ -35,10 +46,10 @@ def crossover(x1, x2, crossoverProb):
 
     return ''
 
-def differential_evolution(function, generations=1000, lenght=50, population=50, crossoverProb=0.3, mutationCons=0.5):
+def differential_evolution(function, generations=1000, bound=5.12, population=20, dimension=10, crossoverProb=0.3, mutationCons=0.5):
 
     # Pseudo-random generation of population
-    p = create_first(lenght, population)
+    p = create_first(bound, population, dimension)
     new_p = []
     best = select_best(function, p)  # Select best solution from population
 
@@ -75,9 +86,9 @@ def funcion(s):
 
 if __name__ == '__main__':
 
-    print(differential_evolution(funcion))
+    #print(differential_evolution(funcion))
 
-
+    create_first(5, 5, 8)
 
 
 
